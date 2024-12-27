db3c221158bb4ba7c083cdc6a97ffc6aa2ed1f4f
SalasGith <mosalahkd028@gmail.com>
2024-12-27 11:26:35 +0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
-public class NameClient {
+public class Client {
     public static void main(String[] args) {
         try {
             Socket socket = new Socket("localhost", 3999);
